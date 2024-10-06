ccdf3f5184b0966c2418318993177a522e6c96a8
Vakazzz <vladikglobal@gmail.com>
2024-10-06 14:41:01 +0300

    Initial Commit

---
+++ Project 1Tests/Project_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1Tests.swift
+//  Project 1Tests
+//
+//  Created by Vladislav Alexandrov on 06.10.2024.
+//
+
+import Testing
+@testable import Project_1
+
+struct Project_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
